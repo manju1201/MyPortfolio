7a009747f21d8f466e7238c0a372595cb478d81b
manju1201 <manjju2001@gmail.com>
2024-05-29 16:03:47 -0700

    second push deployement

--- Components/Blog.tsx
+++ Components/Blog.tsx
@@ -1,3 +1,50 @@
+// import { ChatBubbleLeftRightIcon, UserCircleIcon } from "@heroicons/react/20/solid";
+// import Image from "next/image";
+// import React from "react";
+
+// const Blog = () => {
+//   return (
+//     <div className="pt-[4rem] md:pt-[8rem] pb-[4rem] bg-[#02050a]">
+//       <h1 className="heading">
+//         My <span className="text-yellow-400">Publication</span>
+//       </h1>
+//       <div className="grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 mt-[4rem] gap-[3rem] w-[70%] mx-auto">
+//         <div>
+//           <div className="w-[100%] relative h-[400px] rounded-full">
+//             <Image
+//               src="/images/conference-paper.jpg"
+//               alt="blog"
+//               layout="fill"
+//               className="object-cover"
+//             />
+//           </div>
+//           <div
+//             className="w-[90%] text-center mx-auto bg-[#09101a] relative 
+//                 p-[2rem] mt-[-1rem]"
+//           >
+//             <div
+//               className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative 
+//                     mt-[-3rem] text-black font-semibold text-[17px] mx-auto"
+//             >
+//               December 15, 2022
+//             </div>
+//             <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
+//                 <div className="flex items-center">
+//                     <UserCircleIcon className="w-[2rem] mx-auto h-[2rem] text-[#55e6a5]"/>
+//                     <p className="text-white">By Manjju Shree Devy Gendeti</p>
+//                 </div>
+//             </div>
+//           </div>
+//           <p className="mt-[1rem] text-white text-[18px] font-semibold">Polarized Extractive Summarization of Online Product Reviews @ MIKE Conference 2021</p>
+//         </div>
+//               </div>
+//     </div>
+//   );
+// };
+
+// export default Blog;
+
+
 import { ChatBubbleLeftRightIcon, UserCircleIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 import React from "react";
@@ -6,14 +53,13 @@ const Blog = () => {
   return (
     <div className="pt-[4rem] md:pt-[8rem] pb-[4rem] bg-[#02050a]">
       <h1 className="heading">
-        My
-        <span className="text-yellow-400">BLOG</span>
+        My <span className="text-yellow-400">Publication</span>
       </h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] gap-[3rem] w-[80%] mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 mt-[4rem] gap-[3rem] w-[70%] mx-auto">
         <div>
-          <div className="w-[100%] relative h-[400px]">
+          <div className="w-[100%] relative h-[400px] rounded-xl overflow-hidden">
             <Image
-              src="/images/blog3.jpg"
+              src="/images/conference-paper.jpg"
               alt="blog"
               layout="fill"
               className="object-cover"
@@ -21,95 +67,30 @@ const Blog = () => {
           </div>
           <div
             className="w-[90%] text-center mx-auto bg-[#09101a] relative 
-                p-[2rem] mt-[-1rem]"
+                p-[2rem] mt-[-1rem] rounded-b-xl"
           >
             <div
               className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative 
-                    mt-[-3rem] text-black font-semibold text-[17px] mx-auto"
+                    mt-[-3rem] text-black font-semibold text-[17px] mx-auto rounded-full"
             >
-              December 10, 2023
+              December 15, 2022
             </div>
-            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
-                <div className="flex items-center space-x-3">
-                    <UserCircleIcon className="w-[2rem] mx-auto h-[2rem] text-[#55e6a5]"/>
-                    <p className="text-white">By sajal</p>
-                </div>
-                <div className="flex items-center space-x-3 ">
-                    <ChatBubbleLeftRightIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]"/>
-                    <p className="text-white">Comments(12)</p>
-                </div>
+            <div className="flex flex-col md:flex-row items-center justify-center mt-[1rem] space-x-4">
+              <div className="flex items-center">
+                <UserCircleIcon className="w-[2rem] mx-auto h-[2rem] text-[#55e6a5]" />
+                <p className="text-white ml-2">By Manjju Shree Devy Gendeti</p>
+              </div>
             </div>
           </div>
-          <p className="mt-[1rem] text-white text-[18px] font-semibold">Next js Fullstack Development</p>
+          <p className="mt-[1rem] text-white text-center text-[18px] font-semibold">
+            <a  href="https://link.springer.com/chapter/10.1007/978-3-031-21517-9_15" target="_blank">Polarized Extractive Summarization of Online Product Reviews <span className="text-[#55e6a5]">@ MIKE Conference 2021</span></a>
+            
+          </p>
         </div>
-        <div>
-          <div className="w-[100%] relative h-[400px]">
-            <Image
-              src="/images/blog2.jpg"
-              alt="blog"
-              layout="fill"
-              className="object-cover"
-            />
-          </div>
-          <div
-            className="w-[90%] text-center mx-auto bg-[#09101a] relative 
-                p-[2rem] mt-[-1rem]"
-          >
-            <div
-              className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative 
-                    mt-[-3rem] text-black font-semibold text-[17px] mx-auto"
-            >
-              December 10, 2023
-            </div>
-            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
-                <div className="flex items-center space-x-3">
-                    <UserCircleIcon className="w-[2rem] mx-auto h-[2rem] text-[#55e6a5]"/>
-                    <p className="text-white">By sajal</p>
-                </div>
-                <div className="flex items-center space-x-3 ">
-                    <ChatBubbleLeftRightIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]"/>
-                    <p className="text-white">Comments(12)</p>
-                </div>
-            </div>
-          </div>
-          <p className="mt-[1rem] text-white text-[18px] font-semibold">Next js Fullstack Development</p>
-        </div>
-        <div>
-          <div className="w-[100%] relative h-[400px]">
-            <Image
-              src="/images/blog1.jpg"
-              alt="blog"
-              layout="fill"
-              className="object-cover"
-            />
-          </div>
-          <div
-            className="w-[90%] text-center mx-auto bg-[#09101a] relative 
-                p-[2rem] mt-[-1rem]"
-          >
-            <div
-              className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative 
-                    mt-[-3rem] text-black font-semibold text-[17px] mx-auto"
-            >
-              December 10, 2023
-            </div>
-            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
-                <div className="flex items-center space-x-3">
-                    <UserCircleIcon className="w-[2rem] mx-auto h-[2rem] text-[#55e6a5]"/>
-                    <p className="text-white">By sajal</p>
-                </div>
-                <div className="flex items-center space-x-3 ">
-                    <ChatBubbleLeftRightIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]"/>
-                    <p className="text-white">Comments(12)</p>
-                </div>
-            </div>
-          </div>
-          <p className="mt-[1rem] text-white text-[18px] font-semibold">Next js Fullstack Development</p>
-        </div>
-
       </div>
     </div>
   );
 };
 
 export default Blog;
+
--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,63 +1,153 @@
+// import {
+//   DevicePhoneMobileIcon,
+//   EnvelopeIcon,
+//   MapIcon,
+// } from "@heroicons/react/20/solid";
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+// import React from "react";
+
+// const Footer = () => {
+//   return (
+//     <div className="pt-[8rem] pb-[4rem] bg-[#02050a]">
+//       <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem]">
+//       <div className="flex items-start space-x-6">
+//   <div className="flex flex-col items-center space-y-4">
+//     <div className="flex items-center space-x-2">
+//       <div className="md:w-[1.5rem] md:h-[1.5rem] w-[1.5rem] h-[1.5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+//         <FontAwesomeIcon icon={faGithub} className="text-black" />
+//       </div>
+//       <p className="text-[10px] text-white opacity-60">
+//         <a href="https://github.com/manju1201" target="_blank" rel="noopener noreferrer">
+//           GitHub
+//         </a>
+//       </p>
+//     </div>
+//     <div className="flex items-center space-x-2">
+//       <div className="md:w-[1.5rem] md:h-[1.5rem] w-[1.5rem] h-[1.5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+//         <FontAwesomeIcon icon={faLinkedin} className="text-black" />
+//       </div>
+//       <p className="text-[10px] text-white opacity-60">
+//         <a href="https://www.linkedin.com/in/manju-shree/" target="_blank" rel="noopener noreferrer">
+//           LinkedIn
+//         </a>
+//       </p>
+//     </div>
+//   </div>
+// </div>
+
+
+
+//         <div className="flex items-center space-x-6">
+//           <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+//             <DevicePhoneMobileIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
+//           </div>
+//           <div>
+//             <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
+//               Phone
+//             </h1>
+//             <p className="text-[17px] w-[90%] text-white opacity-60">
+//               +16024590415
+//             </p>
+//           </div>
+//         </div>
+//         <div className="flex items-center space-x-6">
+//           <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+//             <EnvelopeIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
+//           </div>
+//           <div>
+//             <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
+//               Email Me
+//             </h1>
+//             <p className="text-[17px] w-[90%] text-white opacity-60">
+//               mgendeti@asu.edu 
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="w-[80%] mt-[2rem] mx-auto flex items-center justify-between">
+// <div className="text-[16px] text-[#55e6a5] opacity-90">
+//     © 2024 Manjju Shree | All rights reserved. 
+//   </div>
+//   <div className="flex items-center space-x-10">
+//     <p className="text-[16px] text-[#55e6a5] opacity-90 text-right">Thank you for visiting my Portfolio</p>
+//   </div>
+// </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+
+
+
+
 import {
   DevicePhoneMobileIcon,
   EnvelopeIcon,
-  MapIcon,
+  GlobeAltIcon
 } from "@heroicons/react/20/solid";
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+
 import React from "react";
 
 const Footer = () => {
   return (
     <div className="pt-[8rem] pb-[4rem] bg-[#02050a]">
-      <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem]">
-        <div className="flex items-center space-x-6">
-          <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
-            <MapIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
+      <div className="flex justify-around border-b-[1px] pb-[6rem] border-gray-400 w-[80%] mx-auto gap-[3rem]">
+        <div className="flex items-center space-x-4">
+          <div className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+            <FontAwesomeIcon icon={faGithub} className="text-black w-[1.5rem] h-[1.5rem]" />
+          </div>
+          <p className="text-[17px] text-white opacity-60">
+            <a href="https://github.com/manju1201" target="_blank" rel="noopener noreferrer">
+              GitHub <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </a>
+          </p>
+        </div>
+        <div className="flex items-center space-x-4">
+          <div className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+            <FontAwesomeIcon icon={faLinkedin} className="text-black w-[1.5rem] h-[1.5rem]" />
+          </div>
+          <p className="text-[17px] text-white opacity-60">
+            <a href="https://www.linkedin.com/in/manju-shree/" target="_blank" rel="noopener noreferrer">
+              LinkedIn <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </a>
+          </p>
+        </div>
+        <div className="flex items-center space-x-4">
+          <div className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+            <DevicePhoneMobileIcon className="w-[1.5rem] h-[1.5rem] text-black" />
           </div>
           <div>
-            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
-              Address
-            </h1>
-            <p className="text-[17px] w-[90%] text-white opacity-60">
-              Lorem, ipsum dolor sit
+            <p className="text-[17px] text-white opacity-60">
+              +1 (602) 459-0415
             </p>
           </div>
         </div>
-        <div className="flex items-center space-x-6">
-          <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
-            <DevicePhoneMobileIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
+        <div className="flex items-center space-x-4">
+          <div className="w-[2.5rem] h-[2.5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+            <EnvelopeIcon className="w-[1.5rem] h-[1.5rem] text-black" />
           </div>
           <div>
-            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
-              Phone
-            </h1>
-            <p className="text-[17px] w-[90%] text-white opacity-60">
-              +16024590415
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center space-x-6">
-          <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
-            <EnvelopeIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
-          </div>
-          <div>
-            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
-              Send Me Email
-            </h1>
-            <p className="text-[17px] w-[90%] text-white opacity-60">
-              mgendeti@asu.edu <br />
-              manjjugendeti@gmail.com
+            <p className="text-[17px] text-white opacity-60">
+              <a href="mailto:mgendeti@asu.edu">mgendeti@asu.edu</a>
             </p>
           </div>
         </div>
       </div>
-      <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between ">
-        <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20">
-          Webdev warriors 2023 | All Rights Reserved
+      <div className="w-[80%] mt-[2rem] mx-auto flex items-center justify-between">
+        <div className="text-[16px] text-[#55e6a5] opacity-90">
+          © 2024 Manjju Shree | All rights reserved. 
         </div>
         <div className="flex items-center space-x-10">
-          <p className="text-[16px] text-white opacity-20">Terms & Condition</p>
-          <p className="text-[16px] text-white opacity-20">Privacy Policy</p>
-          <p className="text-[18px] text-white opacity-20">Sitemap</p>
+          <p className="text-[16px] text-[#55e6a5] opacity-90 text-right">Thank you for visiting my Portfolio</p>
         </div>
       </div>
     </div>
